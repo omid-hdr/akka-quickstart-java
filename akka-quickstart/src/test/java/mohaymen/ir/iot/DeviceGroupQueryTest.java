389fa0e808ded8b510399f8989298ce1acf3a443
omid <omidheydari2004@gmail.com>
2024-07-24 11:46:51 +0330

    add testReturnTemperatureReadingEvenIfDeviceStopsAfterAnswering

--- akka-quickstart/src/test/java/mohaymen/ir/iot/DeviceGroupQueryTest.java
+++ akka-quickstart/src/test/java/mohaymen/ir/iot/DeviceGroupQueryTest.java
@@ -122,5 +122,39 @@ class DeviceGroupQueryTest {
         assertEquals(expectedTemperatures, response.temperatures);
     }
 
+    @Test
+    public void testReturnTemperatureReadingEvenIfDeviceStopsAfterAnswering() {
+        TestKit requester = new TestKit(system);
+
+        TestKit device1 = new TestKit(system);
+        TestKit device2 = new TestKit(system);
+
+        Map<ActorRef, String> actorToDeviceId = new HashMap<>();
+        actorToDeviceId.put(device1.getRef(), "device1");
+        actorToDeviceId.put(device2.getRef(), "device2");
+
+        ActorRef queryActor =
+                system.actorOf(
+                        DeviceGroupQuery.props(
+                                actorToDeviceId, 1L, requester.getRef(), new FiniteDuration(3, TimeUnit.SECONDS)));
+
+        assertEquals(0L, device1.expectMsgClass(Device.ReadTemperature.class).requestId);
+        assertEquals(0L, device2.expectMsgClass(Device.ReadTemperature.class).requestId);
+
+        queryActor.tell(new Device.RespondTemperature(0L, Optional.of(1.0)), device1.getRef());
+        queryActor.tell(new Device.RespondTemperature(0L, Optional.of(2.0)), device2.getRef());
+        device2.getRef().tell(PoisonPill.getInstance(), ActorRef.noSender());
+
+        DeviceGroup.RespondAllTemperatures response =
+                requester.expectMsgClass(DeviceGroup.RespondAllTemperatures.class);
+        assertEquals(1L, response.requestId);
+
+        Map<String, DeviceGroup.TemperatureReading> expectedTemperatures = new HashMap<>();
+        expectedTemperatures.put("device1", new DeviceGroup.Temperature(1.0));
+        expectedTemperatures.put("device2", new DeviceGroup.Temperature(2.0));
+
+        assertEquals(expectedTemperatures, response.temperatures);
+    }
+
 
 }
